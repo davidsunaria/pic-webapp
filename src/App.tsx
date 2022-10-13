e6d03a17a004659aa7fd4fb2c9b87cf8ff6b7a46
Mukesh Kaushal <mukesh@shinewebservices.com>
2022-10-13 15:29:55 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log('dev')
+  //console.log('dev')
   return (
     <div className="App">
       <header className="App-header">
